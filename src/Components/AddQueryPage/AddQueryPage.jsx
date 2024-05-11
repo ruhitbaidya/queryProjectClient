84492079d1542b2f82123fc16c2e022c6a2d9b03
ruhitbaidya <ruhitbaidya01@gmail.com>
2024-05-11 09:59:29 +0600

    finish product create frond end and backend work

--- src/Components/AddQueryPage/AddQueryPage.jsx
+++ src/Components/AddQueryPage/AddQueryPage.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import { AuthUserContext } from "../../AuthContext/AuthContext";
-import times from "../CurrentDateAndTime/DateAtime";
 import axios from "axios";
-
+import DateAtime from "../CurrentDateAndTime/DateAtime";
+import { ToastContainer, toast } from 'react-toastify';
 const AddQueryPage = () => {
     const {user} = useContext(AuthUserContext)
     const handelAddSubmit = (e)=>{
@@ -13,17 +13,23 @@ const AddQueryPage = () => {
         const productImageurl = form.productImageurl.value;
         const queryTItle = form.queryTItle.value;
         const boycottingDetails = form.boycottingDetails.value;
-        const userinfotime = {userEmail : user.email, cName : user.displayName, Uphoto : user.photoURL, recommendationCount : 0, currentTime : times}
+        const userinfotime = {userEmail : user.email, cName : user.displayName, Uphoto : user.photoURL, recommendationCount : 0, currentTime : DateAtime()}
         const infos = {productName, productBrand, productImageurl, queryTItle, boycottingDetails, userinfotime}
         console.log(infos)
-        
+
         axios.post("http://localhost:5000/createProduct", infos)
-        .then((res)=> console.log(res))
+        .then((res)=> {
+            if(res.data.insertedId){
+                toast.success("successfully Create Product")
+                form.reset()
+            }
+        })
     }
     return (
         <div className="my-[50px]">
             <div className="container mx-auto px-[20px]">
                 <div>
+                    <ToastContainer />
                     <h2 className="text-4xl font-[600] text-center">Add Query for Better Alternatives</h2>
                 </div>
                 <div className="mt-[60px]"> 
@@ -31,27 +37,27 @@ const AddQueryPage = () => {
                         <div className="grid lg:grid-cols-3 gap-[30px] mt-[20px]">
                             <div className="">
                                 <label htmlFor="">Product Name</label>
-                                <input className="w-full focus:outline-none px-[10px] py-[10px] bg-gray-200" name="productName"  type="text" placeholder="Product Name" />
+                                <input required className="w-full focus:outline-none px-[10px] py-[10px] bg-gray-200" name="productName"  type="text" placeholder="Product Name" />
                             </div>
                             <div>
                                 <label htmlFor="">Product Brand</label>
-                                <input className="w-full focus:outline-none px-[10px] py-[10px] bg-gray-200" name="productBrand"  type="text" placeholder="Product Brand" />
+                                <input required className="w-full focus:outline-none px-[10px] py-[10px] bg-gray-200" name="productBrand"  type="text" placeholder="Product Brand" />
                             </div>
                             <div>
                                 <label htmlFor="">Product Image-URL</label>
-                                <input className="w-full focus:outline-none px-[10px] py-[10px] bg-gray-200" name="productImageurl"  type="text" placeholder="Product Image-URL" />
+                                <input required className="w-full focus:outline-none px-[10px] py-[10px] bg-gray-200" name="productImageurl"  type="text" placeholder="Product Image-URL" />
                             </div>
                         </div>
                         <div className="grid lg:grid-cols-1 gap-[30px] mt-[20px]">
                             <div className="">
                                 <label htmlFor="">Query TItle</label>
-                                <input className="w-full focus:outline-none px-[10px] py-[10px] bg-gray-200" name="queryTItle"  type="text" placeholder="Query TItle" />
+                                <input required className="w-full focus:outline-none px-[10px] py-[10px] bg-gray-200" name="queryTItle"  type="text" placeholder="Query TItle" />
                             </div>
                         </div>
                         <div className="mt-[20px]">
                             <div className="">
                                 <label htmlFor="">Boycotting Reason Details</label>
-                                <textarea className="w-full focus:outline-none px-[10px] py-[10px] bg-gray-200" name="boycottingDetails" id="" cols="30" rows="5" placeholder="Boycotting Reason Details"></textarea>
+                                <textarea required className="w-full focus:outline-none px-[10px] py-[10px] bg-gray-200" name="boycottingDetails" id="" cols="30" rows="5" placeholder="Boycotting Reason Details"></textarea>
                             </div>
                         </div>
                         <div className="mt-[20px]">
--- src/Components/CurrentDateAndTime/DateAtime.jsx
+++ src/Components/CurrentDateAndTime/DateAtime.jsx
@@ -1,3 +1,6 @@
+
+
+const DateAtime = () => {
 const date = new Date();
 const month = date.getMonth() + 1;
 const day = date.getDate();
@@ -8,5 +11,7 @@ const munit = date.getMinutes();
 const meridiem = hours >= 12 ? 'PM' : 'AM';
 
 const times = `${day <= 9 ? `0${day}` : `${day}`}/${month <= 9 ? `0${month}` : `${month}`}/${year},  ${hours <= 9 ? `0${hours}` : `${hours}`}:${munit <= 9 ? `0${munit}` : `${munit}`} ${meridiem}`
+  return times;
+}
 
-export default times;
+export default DateAtime
